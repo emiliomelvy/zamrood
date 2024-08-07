f683ef63d8c38d342d8925f7d018b5ef54a9df8f
emiliomelvy <melvyemilio@yahoo.co.id>
2024-08-07 23:31:43 +0700

    feat: navbar mobile ui & functionality

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "dark-aquaman": "#0B7373",
+        "dark-teal": "#004040",
+      },
     },
   },
   plugins: [],
